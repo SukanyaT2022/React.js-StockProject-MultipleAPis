1248be86209040fd0bb550e6162733d909a8d52b
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-10-19 13:59:50 -0400

    add

--- src/Stock.js
+++ src/Stock.js
@@ -2,30 +2,51 @@ import React, { useEffect, useState } from 'react'
 
 const Stock = () => {
 const [data, setData]=useState()
-let key = "AYPOQ7UULZ1V58UL";
-let stock = "Meta";
-let URL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stock}&apikey=AYPOQ7UULZ1V58UL`
-    useEffect(()=>{
-      fetch(URL)
-      .then((response)=>response.json())
-      .then(data=>{
-        console.log(data)
-      })
-      fetch(URL)
-      .then((response)=>response.json())
-      .then(data=>{
-        console.log(data)
-      })
-      fetch(URL)
-      .then((response)=>response.json())
-      .then(data=>{
-        console.log(data)
-      })
-      data && console.log(data)
-    },[])
+const [search, setSearch]=useState()
 
+const [stockList,setStockList] = useState([]);
+
+let key = "AYPOQ7UULZ1V58UL";
+let URL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${search}&apikey=key
+`
+    const searchHandler = ()=>{
+      fetch(URL)
+      .then((response)=>response.json())
+      .then(data=>{
+        data["Global Quote"]['05. price'];
+      })
+    }
+
+    useEffect(()=>{
+      fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=key`)
+      .then((response)=>response.json())
+      .then(data=>{
+        // console.log(data["Global Quote"]['05. price'])
+        setStockList(data["Global Quote"]['05. price'])
+      })
+      fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=key`)
+      .then((response)=>response.json())
+      .then(data=>{
+        console.log(data["Global Quote"]['05. price'])
+        setStockList((prev)=>[...prev,...data["Global Quote"]['05. price']])
+      })
+    },[])
+    
   return (
     <div> 
+      {/* <input type='text' 
+          value={search} 
+          onChange = {(e)=>setSearch(e.target.value)} 
+          placeholder='Please enter stock name'
+        />
+      <button onClick={searchHandler}>Search</button> */}
+
+      <div>
+        <h3>Apple: ${stockList && stockList}</h3> 
+        <h3>Microsoft</h3>
+        <h3>Meta</h3>
+      </div>
+
     </div>
   )
 }
