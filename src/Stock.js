8f4150c48ec67f6f8c524101754d8cc3a0d25934
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-10-25 15:57:24 -0400

    add

--- src/Stock.js
+++ src/Stock.js
@@ -1,54 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Stock = () => {
-const [data, setData]=useState()
-const [search, setSearch]=useState()
+  const [data, setData] = useState();
+  const [search, setSearch] = useState("");
+  const [stockList, setStockList] = useState([]);
+  const apiKey = "cRfppcxRAwpQqZqnBxEGkgHT22KwIQ2i";
+  const stocks = ["AAPL", "MSFT", "NVDA", "DIS", "COST"];
+  const endpoint = 'v2/aggs/ticker/';
+  // const item = "AAPL";
 
-const [stockList,setStockList] = useState([]);
 
-let key = "AYPOQ7UULZ1V58UL";
-let URL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${search}&apikey=key
-`
-    const searchHandler = ()=>{
-      fetch(URL)
-      .then((response)=>response.json())
-      .then(data=>{
-        data["Global Quote"]['05. price'];
-      })
-    }
+  useEffect(() => {
+    //loop .forEach()like map
+    stocks.forEach((item) => {
+      fetch(
+        `https://api.polygon.io/${endpoint}${item}/prev?unadjusted=true&apiKey=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.results[0].h)
+          const price = data.results[0].h;
+          stockList && setStockList((prevStockList) => [...prevStockList, price]);
+        });
+    });
+  }, []);
 
-    useEffect(()=>{
-      fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=key`)
-      .then((response)=>response.json())
-      .then(data=>{
-        // console.log(data["Global Quote"]['05. price'])
-        setStockList(data["Global Quote"]['05. price'])
-      })
-      fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=key`)
-      .then((response)=>response.json())
-      .then(data=>{
-        console.log(data["Global Quote"]['05. price'])
-        setStockList((prev)=>[...prev,...data["Global Quote"]['05. price']])
-      })
-    },[])
-    
+  // const searchHandler = () => {
+  //   if (search) {
+  //     fetch(
+  //       `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${search}&apikey=${key}`
+  //     )
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const price = data["Global Quote"]["05. price"];
+  //         setStockList((prevStockList) => [...prevStockList, price]);
+  //       });
+  //   }
+  // };
+
   return (
-    <div> 
-      {/* <input type='text' 
-          value={search} 
-          onChange = {(e)=>setSearch(e.target.value)} 
-          placeholder='Please enter stock name'
-        />
-      <button onClick={searchHandler}>Search</button> */}
-
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Please enter stock name"
+      />
+    {/* button */}
       <div>
-        <h3>Apple: ${stockList && stockList}</h3> 
-        <h3>Microsoft</h3>
-        <h3>Meta</h3>
+        {stockList.map((data, index) => (
+          <div>
+            Stock no {index + 1} = {data}
+          </div>
+        ))}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Stock
+export default Stock;
